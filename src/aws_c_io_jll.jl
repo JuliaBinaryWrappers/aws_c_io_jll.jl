882205a7074f98c4f5873ae9aff093dc65b34e95
jlbuild <juliabuildbot@gmail.com>
2025-12-10 21:46:47 +0000

    aws_c_io_jll build 0.23.4+0

--- src/aws_c_io_jll.jl
+++ src/aws_c_io_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("aws_c_io")
-JLLWrappers.@generate_main_file("aws_c_io", UUID("13c41daa-f319-5298-b5eb-5754e0170d52"))
+JLLWrappers.@generate_main_file("aws_c_io", Base.UUID("13c41daa-f319-5298-b5eb-5754e0170d52"))
 end  # module aws_c_io_jll
