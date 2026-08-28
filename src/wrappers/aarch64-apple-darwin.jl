f0564e7350337453303a221683c9d676e8d1ded2
jlbuild <juliabuildbot@gmail.com>
2026-08-28 18:52:22 +0000

    aws_c_io_jll build 1.0.0+0

--- src/wrappers/aarch64-apple-darwin.jl
+++ src/wrappers/aarch64-apple-darwin.jl
@@ -4,7 +4,7 @@ export libaws_c_io
 using aws_c_cal_jll
 using aws_c_common_jll
 JLLWrappers.@generate_wrapper_header("aws_c_io")
-JLLWrappers.@declare_library_product(libaws_c_io, "@rpath/libaws-c-io.1.0.0.dylib")
+JLLWrappers.@declare_library_product(libaws_c_io, "@rpath/libaws-c-io.1.0.dylib")
 function __init__()
     JLLWrappers.@generate_init_header(aws_c_cal_jll, aws_c_common_jll)
     JLLWrappers.@init_library_product(
--- src/wrappers/aarch64-linux-musl.jl
+++ src/wrappers/aarch64-linux-musl.jl
@@ -5,7 +5,7 @@ using s2n_tls_jll
 using aws_c_cal_jll
 using aws_c_common_jll
 JLLWrappers.@generate_wrapper_header("aws_c_io")
-JLLWrappers.@declare_library_product(libaws_c_io, "libaws-c-io.so.1.0.0")
+JLLWrappers.@declare_library_product(libaws_c_io, "libaws-c-io.so.1.0")
 function __init__()
     JLLWrappers.@generate_init_header(s2n_tls_jll, aws_c_cal_jll, aws_c_common_jll)
     JLLWrappers.@init_library_product(
